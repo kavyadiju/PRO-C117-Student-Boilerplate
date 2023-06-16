9219faed0e7daa2975ee1311499c56280907d575
kavyadiju <70419997+kavyadiju@users.noreply.github.com>
2023-06-16 07:27:19 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,61 +1,42 @@
 $(document).ready(function(){
-
     console.log('Ready')
 
-    //  Fetch the current date and update it in the DOM
+    let date_time = new Date()
+    let current_date = date_time.toLocaleDateString()
 
+    $('#date').text("Date : " + current_date)
 
+    $('#button').click(function(){
 
+        let review = $('#text').val()
+        console.log(review)
 
-    //  write an event, when Submit button is clicked
-    $('').click(function(){
+        let input_data = {'customer_review' : review}
+        console.log(input_data)
 
-        //  get the text value from the textarea using the 'val()' method
-        let text_value = $('').val()
-
-        //  Convert it to JS object.
-        //  Provide a 'key' here and in write the same in app.py file as well to extract data
-        let input_text = {'' : text_value}
-        console.log(input_text)
-
-        //  ajax request
         $.ajax({
 
-            //  type of web request
-            type : '',
-
-            //  Data to be sent in JSON format
-            data : JSON.stringify(),
-
-            //  type of response expected is json
+            url : "/predict",
+            type : "POST",
+            data : JSON.stringify(input_data),
             dataType : 'json',
-
-            //  contentType
             contentType : 'application/json',
-
-            //  if everything is successful, run this function
             success : function(result){
+                let prediction = result.prediction
+                let emoji_url = result.url
+                console.log(emoji_url)
 
-                // extract prediction and emoticon url from result
+                $('#sentiment').text(prediction)
+                $('#sentiment').show()
 
-
-                //  update the DOM elements
-
-
-                //  show them
-
+                $('#emoji').attr('src' , emoji_url)
+                $('#emoji').show()
             },
-
-            //  if any error, run this function
             error : function(result){
-
                 console.log(result)
             }
         })
 
-
-        //  clearing the textbox after every button push
         $('#text').val("")
-    })
         
-})
+    
